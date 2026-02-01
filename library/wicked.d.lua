8cc37a65fcd4dbc4d4b8c42c5972a0d4c9060de9
Phil Schumann <metaleap@users.noreply.github.com>
2026-02-01 20:02:18 +0100

    ..

--- library/wicked.d.lua
+++ library/wicked.d.lua
@@ -1,15 +1,14 @@
 ---@meta
 
----Very basic example of HTTP library
-local http = {}
+local wicked = {}
 
 ---Make a GET request to the provided URL
 ---@param url string The URL to request
-function http.get(url) end
+function wicked.get(url) end
 
 ---Make a POST request to the provided URL
 ---@param url string The URL to request
 ---@param body string The body of the POST request
-function http.post(url, body) end
+function wicked.post(url, body) end
 
-return http
+return wicked
